1f114e05d85b96ed07c6ab6206a1a2dbacbebb9d
eliseweir <67879336+eliseweir@users.noreply.github.com>
2020-12-01 16:31:57 -0600

    Delete redundant files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth
+    });
+  }
+
+  render() {
+    let heading;
+    if (this.state.authorized === true) {
+      heading = "Contact";
+    } else {
+      heading = "Enter the Password";
+    }
+
+    let login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password"/>
+        <input type="submit"/>
+      </form>
+    );
+
+    const contactInfo = (
+      <ul>
+        <li>
+          client@example.com
+        </li>
+        <li>
+          555.555.5555
+        </li>
+      </ul>
+    );
+
+    return (
+      <div id="authorization">
+        <h1>{heading}</h1>
+        {this.state.authorized?contactInfo:login}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Contact />, 
+  document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
